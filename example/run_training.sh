7a329d3727a43e0e7a0cb7a7d5514d08dbdb4753
Henry Howard <howard.henry.ny@gmail.com>
2026-02-12 11:59:10 -0500

    Update run_training.sh

--- example/run_training.sh
+++ example/run_training.sh
@@ -8,11 +8,11 @@ export MKL_DOMAIN_NUM_THREADS="MKL_DOMAIN_ALL=1, MKL_DOMAIN_BLAS=1"
 export MKL_NUM_STRIPES=1
 export NUMEXPR_NUM_THREADS=1
 
-ulimit -u 524288
-ulimit -n 100000
-ulimit -a
+#ulimit -u 524288
+#ulimit -n 100000
+#ulimit -a
 
-ipcluster start -n=5 --profile-dir=ipypar &
+ipcluster start -n=2 --profile-dir=ipypar &
 sleep 5
 echo `date` "Start training"
 train_sc -c fix_config.yaml
